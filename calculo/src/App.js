42fbba8667663b9b5b3704292123d61e0dcdc1f7
Angel <20230253@ricaldone.edu.sv>
2025-03-19 01:47:04 -0600

    .......

---
+++ calculo/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+    <h1>holaaaa</h1>
+
+
+    <div class="card">
+  <div class="card-body">
+  <form class="d-flex">
+      <input class="form-control me-2" type="text" placeholder="Numero 1" aria-label="Numero 1"/>
+    </form>
+    <br/>
+    <form class="d-flex">
+      <input class="form-control me-2" type="text" placeholder="Numero 2" aria-label="Numero 2"/>
+    </form>
+  </div>
+</div>
+    </>
+  );
+}
+
+export default App;
